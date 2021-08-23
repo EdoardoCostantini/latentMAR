858f022bc825266d839975940f5f7162ae9bc87d
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2021-08-23 12:09:38 +0200

    feature: unique plotting framework in analsysis script for fixed and experimental number of preds

--- code/results_analysis.R
+++ code/results_analysis.R
@@ -11,28 +11,28 @@
 # Load Results ------------------------------------------------------------
 
   inDir <- "../output/"
-
-# number of predictors fixed ----------------------------------------------
-
-  runName <- "20210727_173828"
+  runName <- "20210820_123036"
 
   # Read output
   gg_shape <- readRDS(paste0(inDir, runName, "_res.rds"))
 
+# number of predictors fixed ----------------------------------------------
+
   # Define objects to select data
   parm <- unique(gg_shape$par)[1] # what parameter to plot
-  result <- unique(gg_shape$variable)[1] # what result to plot
+  result <- unique(gg_shape$variable)[2] # what result to plot
   met_cond <- paste0(unique(gg_shape$method),
                      collapse = "|")
   mar_cond <- unique(gg_shape$mar_pred)
   lam_cond <- unique(gg_shape$lambda)
 
-  # Obtain plot
+  # Estiamtes distribution
   plot1 <- gg_shape %>%
     # Subset
     filter(grepl(parm, par)) %>%
     filter(grepl(met_cond, method)) %>%
     filter(grepl(result, variable)) %>%
+    filter(n_mar_pred == 1 & mar_pred == "LV" | n_mar_pred == 4 & mar_pred == "IT") %>%
 
     # Main Plot
     ggplot(aes(x = variable,
@@ -58,11 +58,38 @@
 
   plot1
 
-# number of predictors as exp factor --------------------------------------
+  # FMI and RIV
+  result <- unique(gg_shape$variable)[5] # what result to plot
 
-  # Read output
-  runName <- "20210820_123036"
-  gg_shape <- readRDS(paste0(inDir, runName, "_res.rds"))
+  plot_2 <- gg_shape %>%
+    # Subset
+    filter(grepl(parm, par)) %>%
+    filter(grepl(met_cond, method)) %>%
+    filter(grepl(result, variable)) %>%
+    filter(n_mar_pred == 1 & mar_pred == "LV" | n_mar_pred == 4 & mar_pred == "IT") %>%
+
+    # Main Plot
+    ggplot(aes(x = variable,
+               y = value,
+               group = mar_pred,
+               fill = mar_pred)) +
+    geom_boxplot() +
+
+    # Grid
+    facet_grid(cols = vars(factor(lambda,
+                                  labels = paste0("fl = ", lam_cond)))) +
+    # Format
+    theme(text = element_text(size = 15),
+          plot.title = element_text(hjust = 0.5),
+          axis.text = element_text(size = 10),
+          axis.text.x = element_blank(),
+          axis.title = element_text(size = 15)) +
+    labs(title = paste0(result, " for ", parm),
+         x     = NULL,
+         y     = NULL)
+  plot_2
+
+# number of predictors as exp factor --------------------------------------
 
   # Define objects to select data
   parm <- unique(gg_shape$par)[1] # what parameter to plot
@@ -103,33 +130,3 @@
          y     = NULL)
 
   plot1
-
-  # FMI and RIV
-  result <- unique(gg_shape$variable)[5] # what result to plot
-
-  plot_2 <- gg_shape %>%
-    # Subset
-    filter(grepl(parm, par)) %>%
-    filter(grepl(met_cond, method)) %>%
-    filter(grepl(result, variable)) %>%
-
-    # Main Plot
-    ggplot(aes(x = variable,
-               y = value,
-               group = mar_pred,
-               fill = mar_pred)) +
-    geom_boxplot() +
-
-    # Grid
-    facet_grid(cols = vars(factor(lambda,
-                                  labels = paste0("fl = ", lam_cond)))) +
-    # Format
-    theme(text = element_text(size = 15),
-          plot.title = element_text(hjust = 0.5),
-          axis.text = element_text(size = 10),
-          axis.text.x = element_blank(),
-          axis.title = element_text(size = 15)) +
-    labs(title = paste0(result, " for ", parm),
-         x     = NULL,
-         y     = NULL)
-  plot_2
